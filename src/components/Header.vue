c2c36d6c976af50352173ccd8ad2c6ab481069bf
Matheus Morais <matheusiaromo@gmail.com>
2021-06-05 22:55:40 -0300

    adicionei transição no menu

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,15 +1,12 @@
 <template>
   
-  <header>
-    <nav :class="{active2: isActive}">
-      <a href="">LOGO</a>
-      <button  
-      aria-controls="menu" 
-      aria-expanded="false" 
-      class="burguer" 
-      @click="isActive = !isActive">
-    <span id="hamburguer"></span></button>
-      <ul :class="{active: isActive}" id="menu">
+  <header id="header">
+    <a id="logo" href="">LOGO</a>
+    <nav id="nav" :class="{active: isActive}">
+     <button @click="isActive = !isActive" aria-label="Abrir Menu" id="btn-mobile" aria-haspopup="true" aria-controls="menu" aria-expanded="false">
+        <span id="hamburger"></span>
+      </button>
+      <ul id="menu" role="menu">
         <li><a>Home</a></li>
         <li><a>Portfolio</a></li>
         <li><a>Sobre</a></li>
@@ -32,45 +29,80 @@ export default {
 
 <style scoped>
 
-header {
-  margin: 0 auto;
-  box-shadow: 0px 0px 4px rgba(30, 60, 90, .2);
+#logo {
+  padding-left: 1rem;
 }
 
-nav {
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  margin: 0px 10px;
+#header {
+  box-sizing: border-box;
   height: 70px;
-}
-
-a {
-  padding: 1rem;
-  cursor: pointer;
-  display: block;
-}
-
-li a:hover {
-  background: #42b983;
-  color: #ffffff;
-}
-
-ul {
   display: flex;
-  flex-direction: row;
-  list-style: none;
+  align-items: center;
+  justify-content: space-between;
+  background: #ffffff;
+  box-shadow: 0px 0px 4px rgba(0, 0, 0, 0.1);
 }
 
-.burguer {
+#menu {
+  display: flex;
+  list-style: none;
+  gap: 0.5rem;
+}
+
+#menu a {
+  display: block;
+  padding: 0.5rem;
+}
+
+
+
+#btn-mobile {
   display: none;
 }
 
+@media (max-width: 600px) {
+  #menu {
+    display: block;
+    position: absolute;
+    width: 100%;
+    top: 71px;
+    left: 0;
+    transform: translate(-20px, 0px);
+    background: #ffffff;
+    opacity: 0;
+    z-index: 1000;
+    visibility: hidden;
+    overflow-y: hidden;
+  }
 
-@media (max-width: 630px)
-{
+  #nav.active #menu {
+    opacity: 1;
+    transform: translate(0px, 0px);
+    transition: .3s ease;
+    visibility: visible;
+    overflow-y: auto;
+  }
+   #menu li a:hover {
+    color: #ffffff;
+    background: #42b983;
+  }
 
-  button {
+  #menu li a {
+    left: 0;
+    text-align: center;
+    margin: 0px;
+
+  }
+
+  
+ 
+
+  #menu a {
+    padding: 1rem 0;
+    margin: 0 1rem;
+    border-bottom: 2px solid rgba(0, 0, 0, 0.05);
+  }
+  #btn-mobile {
     display: flex;
     padding: 0.5rem 1rem;
     font-size: 1rem;
@@ -79,69 +111,36 @@ ul {
     cursor: pointer;
     gap: 0.5rem;
   }
-
-  nav {
-    background-color: #ffffff;
+  #hamburger {
+    border-top: 3px solid #42b983;
+    width: 30px;
   }
-  .burguer
-   {
-    display: block;
-    cursor: pointer;
-   }
-
-  #hamburguer {
-    border-top: 4px solid #42b983;
-    display: block;
-  }
-
-  #hamburguer:after,
-  #hamburguer:before
-   {
-    margin-top: 4px;
-    content: "";
+  #hamburger::after,
+  #hamburger::before {
+    content: '';
     display: block;
     width: 30px;
-    height:4px;
-    background-color: #42b983;
+    height: 3px;
+    background: #42b983;
+    margin-top: 5px;
     transition: 0.3s;
     position: relative;
   }
-
-   nav.active2 #hamburger {
+  #nav.active #hamburger {
     border-top-color: transparent;
   }
-  nav.active2 #hamburger:before {
+  #nav.active #hamburger::before {
     transform: rotate(135deg);
   }
-  nav.active2 #hamburger:after {
+  #nav.active #hamburger::after {
     transform: rotate(-135deg);
     top: -7px;
   }
-
-   ul {
-     transition: .3s;
-     position: absolute;
-     top: 72px;
-     left: -100%;
-     flex-direction: column;
-     background-color: #ffffff;
-     z-index: 999;
-    
-   }
-
-   ul li a {
-     width: 100vw;
-     text-align: center;
-     border-bottom: 1px solid rgba(30, 60, 90, .1);
-     border-right: 1px solid rgba(30, 60, 90, .1);
-   }
-
-    .active {
-     left: 0;
-   }
-
+  
+  #nav {
+    padding: 0px;
+  }
 
 }
 
 </style>
-
